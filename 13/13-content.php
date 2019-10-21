b4d01fe4232c31b482c8f674a6f8a7f10739b610
tdc-students <cuongtk@tdc.edu.vn>
2019-10-21 13:16:10 +0700

    first commit

---
+++ 13/13-content.php
@@ -0,0 +1,43 @@
+<section class="question-section">
+    <div class="container">
+    <div class="title">
+        <h1>Question & Concerns</h1>
+    </div>
+    <hr>
+    <div class="content">
+        <div class="row">
+            <div class="col-12 col-md-6"><div id="accordianId" role="tablist" aria-multiselectable="true">
+                <div class="card">
+                    <div class="card-header" role="tab" id="section1HeaderId">
+                        <h5 class="mb-0">
+                            <a data-toggle="collapse" data-parent="#accordianId" href="#section1ContentId" aria-expanded="true" aria-controls="section1ContentId">
+                      Where do I get started?
+                    </a>
+                        </h5>
+                    </div>
+                    <div id="section1ContentId" class="collapse in" role="tabpanel" aria-labelledby="section1HeaderId">
+                        <div class="card-body">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptate quis explicabo voluptatibus nemo architecto error dignissimos culpa quas possimus corrupti itaque officiis, reprehenderit repellat. Sint eius iusto itaque recusandae.
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header" role="tab" id="section2HeaderId">
+                        <h5 class="mb-0">
+                            <a data-toggle="collapse" data-parent="#accordianId" href="#section2ContentId" aria-expanded="true" aria-controls="section2ContentId">
+                            What happens at the end of the parid period ?
+                    </a>
+                        </h5>
+                    </div>
+                    <div id="section2ContentId" class="collapse in" role="tabpanel" aria-labelledby="section2HeaderId">
+                        <div class="card-body">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ad, pariatur aliquam dolore distinctio iste architecto accusamus provident amet atque dolorem, non voluptates fuga unde tempora nobis fugit aliquid dolorum.
+                        </div>
+                    </div>
+                </div>
+            </div></div>
+            <div class="col-12 col-md-6"></div>
+        </div>
+    </div>
+    </div>
+</section>
