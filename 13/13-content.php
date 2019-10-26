8d1eea0559630c24a5b371c1adbba15b2e706163
tuan <tuannt621@gmail.com>
2019-10-26 07:58:42 +0700

    26/10 commit

--- 13/13-content.php
+++
@@ -1,6 +0,0 @@
-<section class="about-me-section py-5 text-center">
-<div class="container">
-    <div class="row">
-        <div class="col-12 col-md-6 h5 text-white" >About Me</div>
-        <div class="col-12 col-md-6 "><small class="text-white">  Home / <span>About Me</span></small></div></div></div>
-</section>
--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
- var swiper = new Swiper('.swiper-container', {
-    navigation: {
-       nextEl: '.flex-next',
-      prevEl:  '.flex-prev',
-    },
-  });
-});
