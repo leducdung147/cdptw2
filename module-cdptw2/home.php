565e35f1f806cad6b026bebe30f64c3dbee51a04
leducdung147 <leducdung147@gmail.com>
2019-10-30 22:56:39 +0700

    update module 16

--- module-cdptw2/home.php
+++ module-cdptw2/home.php
@@ -20,6 +20,7 @@
     <link rel="stylesheet" href="./14/scss/14.css" >
     <link rel="stylesheet" href="./18/scss/18.css" >
     <link rel="stylesheet" href="./19/scss/19.css" >
+    <link rel="stylesheet" href="./16/scss/16.css" >
     <link href="./css/swiper.min.css" rel="stylesheet" type="text/css" />
 
 </head>
@@ -33,7 +34,7 @@
       <?php include "./11/11-content.php"; ?>
       <?php include "./12/12-content.php"; ?>
       <?php include "./13/13-content.php"; ?>
-     
+      <?php include "./16/16-content.php"; ?>
       <div class="posts py-5">
       <div class="container">
       <div class="row">
@@ -48,12 +49,12 @@
   </body>
 </html>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
    
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src='./js/swiper.min.js'></script>
     <script type="module" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.esm.js"></script>
     <script src="./js/script_13.js"></script>
+    <script src="./js/isotope.pkgd.min.js"></script>
 <script>
 
     var swiper1 = new Swiper('.swiper1', {
@@ -222,5 +223,28 @@
         }
         }
     });
+
+</script>
+<script>
+
+$grid = $('.grid');
+$grid.isotope();
+
+$('#food-button,#staff-button, #all-button').click(function() {
+
+  var id = $(this).attr('id');
+  var className = id.replace("-button", "");
+
+  if (className == 'all') {
+    $grid.isotope({
+      filter: '*'
+    });
+    return false;
+  }
+  $grid.isotope({
+    filter: '.' + className
+  });
+});
+
 
 </script>
