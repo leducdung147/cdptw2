22b38e7f5380ad8133521ebc8e6da3a48fea7e96
Thanh Truyền <lorenboy99@gmail.com>
2019-10-31 10:49:20 +0700

    Update module 31 32

--- module-cdptw2/32/32.php
+++ module-cdptw2/32/32.php
@@ -1,3 +1,45 @@
 <div class="module-32__container container">
- 
+    <div class="woo-tabs-panel-comment">
+        <div class="comment">
+            <h2 class="comment_title">Reviews</h2>
+            <p class="noreviews">There are no reviews yet.</p>
+            <p class="firts_to_review">Be the first to review "The Meat Free Monday Cookbook"</p>
+            <p class="comment_note">Your email address will not be published. Required fields are marked *</p>
+        </div>
+        <div class="review_form_wrapper">
+            <p class="your_rating">Your Rating</p>
+            <div id="rating">
+                <input type="radio" id="star5" name="rating" value="5" />
+                <label class="full" for="star5"></label>
+
+                <input type="radio" id="star4" name="rating" value="4" />
+                <label class="full" for="star4"></label>
+
+                <input type="radio" id="star3" name="rating" value="3" />
+                <label class="full" for="star3"></label>
+
+                <input type="radio" id="star2" name="rating" value="2" />
+                <label class="full" for="star2"></label>
+
+                <input type="radio" id="star1" name="rating" value="1" />
+                <label class="full" for="star1"></label>
+            </div>
+            <div class="comment-form-comment">
+                <p>Your Review *</p>
+                <textarea name="comment" id="comment" cols="45" rows="8" aria-required="true"></textarea>
+            </div>
+            <div class="comment-form-author">
+                <span>Name *</span>
+                <input type="text" size="30" aria-required="true">
+            </div>
+            <div class="comment-form-email">
+                <span>Email *</span>
+                <input type="text" size="30" aria-required="true">
+            </div>
+            <button class="btn-review-submit" type="submit">Submit</button>
+        </div>
+
+
+    </div>
+
 </div>
