21dc7c25498927ebdda7268677863ca47006ab84
Thanh Truyền <lorenboy99@gmail.com>
2019-11-02 10:33:17 +0700

    Update module 34

--- module-cdptw2/34/34.php
+++ module-cdptw2/34/34.php
@@ -1,18 +1,42 @@
-<div class="module-34__container container">
-    <div class="form-contact-page">
+<div class="module-34__container">
+
+    <div class="form-contact-page  container">
         <h2 class="form_contact_title">Contact Us Today</h2>
         <form action="#" method="post">
             <div class="contact_columns_count_3">
-                <div class="contact_columns_item-1">
-                    <input type="text" placeholder="Name *">
+
+                <div class="row">
+
+                    <div class="col-md-4 ">
+                        <div class="contact_columns_item-1">
+                            <input type="text" placeholder="Name *">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+
+                        <div class="contact_columns_item-2">
+                            <input type="text" placeholder="Email *">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="contact_columns_item-3">
+                            <input type="text" placeholder="Subject">
+                        </div>
+                    </div>
                 </div>
-                <div class="contact_columns_item-2">
-                    <input type="text" placeholder="Email *">
+                <div class="form_mesenge_form">
+                    <div class="contact_columns_item-4 clear">
+                        <textarea name="messenge" id="messenge" cols="45" rows="5" placeholder="Messenge"></textarea>
+                    </div>
+                    <div class="contact_button_form_submit">
+                        <button type="submit">Send Messenge</button>
+                    </div>
                 </div>
-                <div class="contact_columns_item-3">
-                    <input type="text" placeholder="Subject">
-                </div> 
+
+
+
             </div>
+
         </form>
     </div>
 </div>
